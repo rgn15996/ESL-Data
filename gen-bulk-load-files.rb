69fdb324c598c759fff001b2cb88b6928425359e
Richard Noble <rgn15996@gmail.com>
2014-01-27 19:40:40 +0000

    Create HSN mappings in bulk load

--- gen-bulk-load-files.rb
+++ gen-bulk-load-files.rb
@@ -11,39 +11,41 @@ require "#{File.dirname(__FILE__)}/esldata"
 
 srvs = Array.new
 sols = Array.new
+solmap = Array.new
 
 # open nodes file for writing
 
 ci_file = File.open('ci.csv', "w")
 sol_file = File.open('sol.csv', "w")
+sol_rel_file = File.open('sol_rels.csv', "w")
 
 # Load basic CI data into an array
 ESLdata.load_servers(srvs)
 
-puts "Server fields"
-sizy = 0
-srvs[0].each do |key,value|
-  puts "#{sizy} #{key}"
-  sizy += 1
-end
+# puts "Server fields"
+# sizy = 0
+# srvs[0].each do |key,value|
+#   puts "#{sizy} #{key}"
+#   sizy += 1
+# end
 
 # Load business solution names (HSNs)
 ESLdata.load_business_solutions(sols)
 
-puts "Solution fields"
+# puts "Solution fields"
 
-sizy = 0
-sols[0].each do |key,value|
+# sizy = 0
+# sols[0].each do |key,value|
 
-  puts "#{sizy} #{key}"
-  sizy += 1
-end
+#   puts "#{sizy} #{key}"
+#   sizy += 1
+# end
 
+srv_length = srvs.length
 puts "There are #{srvs.length} servers, #{srvs.uniq.length} unique"
 puts "There are #{sols.length} business solutions, #{sols.uniq.length} unique"
 
-puts "Writing CI header row"
-puts "class is #{srvs[0].length}"
+puts "Write CI header row"
 
 ci_file.write("l:label\t")
 indx = 1
@@ -57,10 +59,10 @@ srvs[0].each do |key,value|
 	indx += 1
 end
 
-#ci_file.write("name\tl:label\tstatus\n")
+puts "Write CI data..."
 
 srvs.each_with_index do |srv, index|
-	ci_file.write("CI\t")
+	ci_file.write("ci\t")
 	indx = 1
 	srv.each do |key,value|
 		ci_file.write("#{value}")
@@ -73,6 +75,66 @@ srvs.each_with_index do |srv, index|
 	end
 end
 
+
+puts "Write Sol header row"
+
+sol_file.write("l:label\t")
+indx = 1
+sols[0].each do |key,value|
+	sol_file.write("#{key}")
+	if indx < sols[0].length then
+		sol_file.write("\t")
+	else
+		sol_file.write("\n")
+	end
+	indx += 1
+end
+
+puts "Write Solution data..."
+
+sols.each_with_index do |sol, index|
+	sol_file.write("solution\t")
+	indx = 1
+	sol.each do |key,value|
+		sol_file.write("#{value}")
+		if indx < sols[0].length then
+			sol_file.write("\t")
+		else
+			sol_file.write("\n")
+		end
+		indx += 1
+	end
+end
 ci_file.close()
 sol_file.close()
 
+# Load solution to ci mappings
+ESLdata.load_solutions(solmap)
+puts "Solution fields"
+
+sizy = 0
+solmap[0].each do |key,value|
+
+  puts "#{sizy} #{key}"
+  sizy += 1
+end
+
+solmap.each do |inst|
+	srvs.each_with_index do |srv, indx_srv|
+		if srv[:full_node_name] == inst[:full_node_name] then
+			# found the server in the srvs array
+			sols.each_with_index do |sol, indx_sol|
+				if sol[:business_service] == inst[:solution_name] then
+					sol_rel_file.write(indx_srv)
+					sol_rel_file.write("\t")
+					sol_rel_file.write(indx_sol+srv_length)
+					sol_rel_file.write("\t")
+					sol_rel_file.write("IS_COMPONENT_OF")
+					sol_rel_file.write("\n")
+			  end
+			end
+		end
+	end
+end
+
+sol_rel_file.close()
